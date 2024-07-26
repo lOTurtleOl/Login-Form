0f02bad33e592d74ea11d716b4189093d385e16d
Andrew Hughes <andyashmore1@gmail.com>
2024-07-26 12:53:15 -0500

    remove state update, need reset input values

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -2,43 +2,20 @@ import React from 'react';
 import './App.css'
 
 export default class LoginForm extends React.Component {
-    constructor(props) {
-        super(props); // calling the constructor of the parent React.Component
-        this.state = {
-            userName: '',
-            password: '',
-        };
-    }
-
-    handleChange = (event) => {
-        const {name, value} = event.target;
-        this.setState({
-            [name]: value
-        });
-    };
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.setState({
-            userName: '',
-            password: ''
-        });
-    };
-
     render() {
         return(
             <div className="App-header">
                 <div>
                     <h3 className="border">Log In</h3>
                 </div>
-                <form className='App' onSubmit={this.handleSubmit}>
+                <form className='App'>
                     <label>
                         Username:
-                        <input type="text" userName="userName" placeholder='Username' value={this.state.userName} onChange={this.handleChange} />
+                        <input type="text" userName="userName" placeholder='Username' />
                     </label>
                     <label>
                         Password:
-                        <input type="password" password="password" placeholder='Password' value={this.state.password} onChange={this.handleChange} />
+                        <input type="password" password="password" placeholder='Password' />
                     </label>                    
                 </form>
                 <div>
